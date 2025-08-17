31196f02e28bc487f75e9ecf1f4f61aec161cda8
biancaalexis <alexissaez725@gmail.com>
2025-08-17 12:52:57 +0800

    Add fishr.css

--- resources/views/landingPage/landing.blade.php
+++ resources/views/landingPage/landing.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>AgriSys - San Pedro City Agriculture Office</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/landing.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fishr.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
