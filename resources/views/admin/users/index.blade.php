b8d0372fa2a9c48e6100663858e5c315f449dbbb
biancaalexis <alexissaez725@gmail.com>
2025-10-25 13:07:43 +0800

    Remove rejected statistic card for  even cards.

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -91,26 +91,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-danger shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Rejected
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="rejected-count">
-                                {{ $stats['rejected'] ?? 0 }}
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-times-circle fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- Enhanced Filters Section -->
