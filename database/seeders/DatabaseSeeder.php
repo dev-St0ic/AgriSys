0c2ccbe22eeec33b812250f55d4ed3bc51803421
Blanca Alexis Saez <alexissaez725@gmail.com>
2026-03-10 12:10:44 +0800

    Fixed the resolved conflic.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,22 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             SuperAdminSeeder::class,
+            // BarangaySeeder::class,
+            // FishrRequestSeeder::class,
+            FisherfolkRegisteredSeeder::class,
+            SuppliesSeeder::class,
+            // SupplyManagementSeeder::class,  // Run before SeedlingRequestSeeder to ensure supplies are available
+            // SeedlingRequestSeeder::class,
+            VegetableSeedlingsDispersalSeeder::class,
+            // BoatrRequestSeeder::class,
+            BoatrRegisteredSeeder::class,
+            // RsbsaApplicationSeeder::class,
+            RsbsaSpecificDataSeeder::class, // Run after RsbsaApplicationSeeder to link specific data to applications
+            TrainingApplicationSeeder::class,
+            UserRegistrationSeeder::class,
+            
             EventSeeder::class,
-            SlideshowImagesSeeder::class,
+            SlideshowImagesSeeder::class
         ]);
     }
 }
