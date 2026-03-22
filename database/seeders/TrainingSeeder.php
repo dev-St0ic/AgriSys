c0f58520bc06797893403f0fa70cb7f9542dc7f4
Blanca Alexis Saez <alexissaez725@gmail.com>
2026-03-22 13:32:54 +0800

    Fixed the generated on the seeder training.

--- database/seeders/TrainingSeeder.php
+++ database/seeders/TrainingSeeder.php
@@ -127,10 +127,20 @@ class TrainingSeeder extends Seeder
 
     private function generateApplicationNumber(): string
     {
-        do {
-            $number = 'TRAIN-' . strtoupper(\Illuminate\Support\Str::random(8));
-        } while (TrainingApplication::where('application_number', $number)->exists());
+    $year = now()->year;
 
-        return $number;
+    $last = TrainingApplication::where('application_number', 'like', "TRAIN-{$year}-%")
+        ->orderByDesc('application_number')
+        ->value('application_number');
+
+    $nextSequence = $last
+        ? (int) substr($last, strrpos($last, '-') + 1) + 1
+        : 1;
+
+    if ($nextSequence > 9999) {
+        throw new \Exception("Training application number limit reached for year {$year}.");
+    }
+
+    return "TRAIN-{$year}-" . str_pad($nextSequence, 3, '0', STR_PAD_LEFT);
     }
 }
